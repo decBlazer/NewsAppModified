9ca99c733020ebd41ea5b9b0fd32d5dd72dcce62
Blaise Varughese <blaise.varughese@dardoc.com>
2024-06-04 23:23:49 -0500

    Part 7 done (Onboarding ViewModel)

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -28,7 +28,9 @@ import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun OnBoardingScreen() {
+fun OnBoardingScreen(
+    event: (OnBoardingEvent) -> Unit
+) {
     Column(modifier = Modifier.fillMaxSize()) {
         val pagerState = rememberPagerState(initialPage = 0) {
             pages.size
@@ -77,8 +79,8 @@ fun OnBoardingScreen() {
 
                 NewsButton(text = buttonState.value[1], onClick = {
                     scope.launch {
-                        if (pagerState.currentPage == 3) {
-                            // TODO: Navigate to Home Screen
+                        if (pagerState.currentPage == 2) {
+                            event(OnBoardingEvent.SaveAppEntry)
                         } else {
                             pagerState.animateScrollToPage(
                                 page = pagerState.currentPage + 1
