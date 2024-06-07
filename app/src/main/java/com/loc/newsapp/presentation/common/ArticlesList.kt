9b6fa1a56784af8738d45d332f56e95466c01487
Blaise Varughese <blaise.varughese@dardoc.com>
2024-06-06 19:37:33 -0500

    Part 16 - finished bookmark screen

--- app/src/main/java/com/loc/newsapp/presentation/common/ArticlesList.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/ArticlesList.kt
@@ -11,15 +11,40 @@ import androidx.compose.ui.Modifier
 import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.presentation.home.components.ArticleCard
 import com.loc.newsapp.presentation.onboarding.Dimens.ExtraSmallPadding2
 import com.loc.newsapp.presentation.onboarding.Dimens.MediumPadding1
-import com.loc.newsapp.presentation.home.components.ArticleCard
+
+@Composable
+fun ArticlesList(
+    modifier: Modifier = Modifier,
+    articles: List<Article>,
+    onClick: (Article) -> Unit
+) {
+    if (articles.isEmpty()){
+        EmptyScreen()
+    }
+    LazyColumn(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(MediumPadding1),
+        contentPadding = PaddingValues(all = ExtraSmallPadding2)
+    ) {
+        items(
+            count = articles.size,
+        ) {
+            articles[it]?.let { article ->
+                ArticleCard(article = article, onClick = { onClick(article) })
+            }
+        }
+    }
+
+}
 
 @Composable
 fun ArticlesList(
     modifier: Modifier = Modifier,
     articles: LazyPagingItems<Article>,
-    onClick:(Article) -> Unit
+    onClick: (Article) -> Unit
 ) {
 
     val handlePagingResult = handlePagingResult(articles)
@@ -35,7 +60,7 @@ fun ArticlesList(
                 count = articles.itemCount,
             ) {
                 articles[it]?.let { article ->
-                    ArticleCard(article = article, onClick = {onClick(article)})
+                    ArticleCard(article = article, onClick = { onClick(article) })
                 }
             }
         }
@@ -71,8 +96,8 @@ fun handlePagingResult(articles: LazyPagingItems<Article>): Boolean {
 
 @Composable
 fun ShimmerEffect() {
-    Column(verticalArrangement = Arrangement.spacedBy(MediumPadding1)){
-        repeat(10){
+    Column(verticalArrangement = Arrangement.spacedBy(MediumPadding1)) {
+        repeat(10) {
             ArticleCardShimmerEffect(
                 modifier = Modifier.padding(horizontal = MediumPadding1)
             )
--- app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
+++ app/src/main/java/com/loc/newsapp/presentation/navgraph/NavGraph.kt
@@ -6,9 +6,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import com.loc.newsapp.presentation.bookmark.BookmarkScreen
 import com.loc.newsapp.presentation.onboarding.OnBoardingScreen
 import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
-import com.loc.newsapp.presentation.search.SearchScreen
 import com.loc.newsapp.presentation.search.SearchViewModel
 
 @Composable
@@ -38,7 +38,7 @@ fun NavGraph(
         ) {
             composable(route = Route.NewsNavigatiorScreen.route) {
                 val viewModel: SearchViewModel = hiltViewModel()
-                SearchScreen(state =  viewModel.state.value, event = viewModel::onEvent, navigate = {})
+                BookmarkScreen(state = viewModel.state.value, navigate = {})
             }
         }
     }
